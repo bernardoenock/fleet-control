4f495a56401604cd906fbd8b92fdce31728189a3
bernardoenock <bernardoenock@argosmonitoramento.com.br>
2025-11-24 20:02:11 -0300

    feat: errorHandler

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,7 @@
+import type { Request, Response, NextFunction } from "express";
+
+export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+  console.error(err);
+  const status = err.statusCode || 400;
+  res.status(status).json({ error: err.message || "Internal error" });
+}
