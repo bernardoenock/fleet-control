627e860db5ced7b7c3b0e4d8a3724e0f19213d12
bernardoenock <bernardoenock@argosmonitoramento.com.br>
2025-11-24 20:02:40 -0300

    feat: Usages validation

---
+++ src/validations/usages.validation.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const startUsageSchema = z.object({
+  carId: z.number().int(),
+  driverId: z.number().int(),
+  startAt: z.string().datetime().optional(), // accept ISO datetime or set server side
+  reason: z.string().min(3)
+});
