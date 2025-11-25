9d9eba8c47fe792dfc7f2e12a98ed14c3a43946e
bernardoenock <bernardoenock@argosmonitoramento.com.br>
2025-11-24 21:29:55 -0300

    feat: add metods on cars controller

--- src/controllers/cars.controller.ts
+++ src/controllers/cars.controller.ts
@@ -4,16 +4,32 @@ import { CarsService } from "../services/cars.service";
 export class CarsController {
   private service = new CarsService();
 
-  async create(req: Request, res: Response) {
+  create = async (req: Request, res: Response) => {
     const car = await this.service.create(req.body);
     res.status(201).json(car);
-  }
+  };
 
-  async list(req: Request, res: Response) {
+  list = async (req: Request, res: Response) => {
     const { color, brand } = req.query;
     const cars = await this.service.list({ color: color as string, brand: brand as string });
     res.json(cars);
-  }
+  };
 
-  // getById, update, delete similar
+  getById = async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const car = await this.service.getById(id);
+    res.json(car);
+  };
+
+  update = async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const updated = await this.service.update(id, req.body);
+    res.json(updated);
+  };
+
+  delete = async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    await this.service.delete(id);
+    res.status(204).send();
+  };
 }
